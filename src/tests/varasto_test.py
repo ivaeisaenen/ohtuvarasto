53a7670aa111b49137d58937acc84601dfbd808b
unknown <ivaeisaenen@gmail.com>
2022-11-07 22:00:03 +0200

    Kun luokan Varasto (tiedoston src/varasto.py) testien haarautumakattavuus (branch coverage) on 100%, pushaa tekemäsi muutokset GitHubiin

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,42 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_lisaa_varastoon1(self):
+        self.varasto.lisaa_varastoon(-1)
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 10)
+    def test_lisaa_varastoon2(self):
+        self.varasto.lisaa_varastoon(10)
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 0)
+    def test_lisaa_varastoon3(self):
+        self.varasto.lisaa_varastoon(12)
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 0)
+
+    def test_ota_varastosta1(self):
+        self.varasto.ota_varastosta(-1)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+    def test_ota_varastosta2(self):
+        saatu_maara = self.varasto.ota_varastosta(12)
+        self.assertAlmostEqual(saatu_maara, 0)
+        self.assertAlmostEqual(self.varasto.saldo, 0.0)
+
+    def test_init1(self):
+        varasto1 = Varasto(-1, alku_saldo=-1)
+        self.assertAlmostEqual(varasto1.saldo, 0.0)
+        self.assertAlmostEqual(varasto1.tilavuus, 0.0)
+
+    def test_init2(self):
+        varasto1 = Varasto(10, alku_saldo=8)
+        self.assertAlmostEqual(varasto1.saldo, 8)
+        self.assertAlmostEqual(varasto1.tilavuus, 10)
+
+    def test_init3(self):
+        varasto1 = Varasto(10, alku_saldo=12)
+        self.assertAlmostEqual(varasto1.saldo, 10)
+        self.assertAlmostEqual(varasto1.tilavuus, 10)
+
+    def test_str(self):
+        varasto1 = Varasto(10, alku_saldo=1)
+        str = varasto1.__str__()
+        print(str)
+        self.assertTrue(str == "saldo = 1, vielä tilaa 9")
